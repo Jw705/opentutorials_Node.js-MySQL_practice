5ec300ebb4e6222045728d8cef884e614f311a05
Juwan <sirius_75@naver.com>
2022-08-10 22:31:08 +0900

    remove test

--- login.js
+++ login.js
@@ -4,22 +4,19 @@ const session = require('express-session')
 const bodyParser = require('body-parser');
 const FileStore = require('session-file-store')(session)
 
-var account = require('./lib_login/account');
 var authRouter = require('./lib_login/auth');
 var authCheck = require('./lib_login/authCheck.js');
-
 var template = require('./lib_login/template.js');
 
 const app = express()
 const port = 3000
 
-//app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-  secret: 'keyboard cat',         // 버전관리 소스코드에 포함해서는 안됨!
-  resave: false,                  // 세션 데이터가 바뀌기 전까지는 세션 저장소에 값을 저장하지 않는다.
-  saveUninitialized: true,        // 세션이 필요하기 전까지는 세션을 구동시키지 않는다.
-  store:new FileStore(),          // 파일에 세션 저장
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store:new FileStore(),
 }))
 
 app.get('/', (request, response) => {
@@ -37,7 +34,6 @@ app.use('/auth', authRouter);
 
 // 메인 페이지
 app.get('/main', (req, res) => {
-  //account.home(req, res);
   var title = 'Welcome';
   var html = template.HTML(title, ``, ``,
     `<hr>
